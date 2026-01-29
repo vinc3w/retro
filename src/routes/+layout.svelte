a6ef7876c5fe268000c2e4b0da984a80053a069d
vinc3w <vinc3w59@gmail.com>
2026-01-29 21:26:03 +0800

    fix incorrect manifest link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -35,7 +35,7 @@
 	<link rel="apple-touch-icon" sizes="180x180" href="{base}/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="{base}/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="{base}/favicon-16x16.png">
-	<link rel="manifest" href="{base}/avatar.png">
+	<link rel="manifest" href="{base}/site.webmanifest">
 	<title>UwU</title>
 </svelte:head>
 
